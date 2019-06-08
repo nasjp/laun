4b446ff61a05f86d342de15913ce8e8ea49fa605
y.taniguchi <y.softvalley@gmail.com>
2019-06-08 14:52:39 +0900

    fix long message

--- internal/laun/exec.go
+++ internal/laun/exec.go
@@ -13,7 +13,15 @@ func Exec() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "laun",
 		Short: "laun is simple application launcher",
-		Args:  cobra.MinimumNArgs(1),
+		Long: `Plese use this simply.
+You can do this for example with the following command:
+
+   $ laun Docker
+   $ laun docker            // You can use lower case
+   $ laun doc               // You can use a part of apps name
+   $ laun Docker Firefox    // You can luanch multiple applications at the same time
+   $ laun atom -t README.md // You can launch with each file or directory
+   $ laun -l                // You can check your applicatin list`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if ls {
 				err := list()
--- internal/laun/open.go
+++ internal/laun/open.go
@@ -1,15 +1,22 @@
 package laun
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+
 	"github.com/YukihiroTaniguchi/laun/pkg/apps"
 	"github.com/YukihiroTaniguchi/laun/pkg/config"
 )
 
 func open(args []string) error {
+	if len(args) <= 0 {
+		return errors.New(fmt.Sprintf("requires at least 1 arg(s), only received %d", len(args)))
+	}
 	c := config.New()
-
 	for _, arg := range args {
-		a := apps.New(arg, c.AppPath)
+		lArg := strings.ToLower(arg)
+		a := apps.New(lArg, c.AppPath)
 		a.Target = target
 		a = a.First()
 		a.Open()
